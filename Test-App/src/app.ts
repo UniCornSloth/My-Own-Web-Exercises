edf88c2bcb43b3539d69825ae08c724a683819b6
Herling <bothaherling@gmail.com>
2023-05-03 15:43:55 +0200

    update on login form

--- Test-App/src/app.ts
+++ Test-App/src/app.ts
@@ -1,8 +1,40 @@
-function handleSubmit(event: Event) {
-  event.preventDefault();
-  console.log("Button Clicked!");
+// Login form
+interface loginFormInformation {
+  username: string;
+  password: string;
 }
 
-const formSubmit = document.querySelector(".submit");
+// Class for the login form
+class loginForm {
+  private usernameInput: HTMLInputElement;
+  private passwordInput: HTMLInputElement;
+  private form: HTMLFormElement;
 
-formSubmit.addEventListener(".submit", handleSubmit);
+  constructor() {
+    this.usernameInput = document.querySelector('.form input[type="text"]');
+    this.passwordInput = document.querySelector('.form input[type="password"]');
+    this.form = document.querySelector(".form") as HTMLFormElement;
+    this.form.addEventListener("submit", this.onSubmit);
+  }
+
+  private onSubmit = (event: Event) => {
+    event.preventDefault();
+
+    const values: loginFormInformation = {
+      username: this.usernameInput.value,
+      password: this.passwordInput.value,
+    };
+
+    console.log(values);
+  };
+}
+
+// Function for submit button after Username and Passward is filled in
+// function handleSubmit(event: Event) {
+//   event.preventDefault();
+//   console.log("Button Clicked!");
+// }
+
+// const formSubmit = document.querySelector(".submit");
+
+// formSubmit.addEventListener(".submit", handleSubmit);
