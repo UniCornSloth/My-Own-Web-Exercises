80d3764602749caa39652e5bbd4da32648eebf90
Herling <bothaherling@gmail.com>
2023-05-10 15:51:44 +0200

    Update

--- Test-App/src/app.ts
+++ Test-App/src/app.ts
@@ -1,5 +1,6 @@
 // Getting form elements from HTML document/ DOM
 // First fetch all elements from DOM using queryselector and store in constants for use in functions and eventlisteners
+// Elements for the LOGIN FORM
 const form = document.querySelector("#form") as HTMLFormElement;
 const usernameInput = document.querySelector("#username") as HTMLInputElement;
 const passwordInput = document.querySelector("#password") as HTMLInputElement;
@@ -7,9 +8,15 @@ const submitButton = document.querySelector("#submit") as HTMLButtonElement;
 const formContainer = document.querySelector(
   "#form-container"
 ) as HTMLFormElement;
-const header = document.querySelector("#h1") as HTMLElement;
 
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Elements on the Main Nav Bar
+const mainPage = document.querySelector("#main-page") as HTMLElement;
+const portfolioButton = document.querySelector("#portfolio") as HTMLElement;
+const aboutMeButton = document.querySelector("#about-me") as HTMLElement;
+const contactMeButton = document.querySelector("#contact-me") as HTMLElement;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// LOGIN SECTION
 // Function using Regular Expression to define and make sure password is secure must be atleast 8 Characters long.
 function isPasswordSecure(password: string): boolean {
   const passwordRegex =
@@ -17,7 +24,24 @@ function isPasswordSecure(password: string): boolean {
   return passwordRegex.test(password);
 }
 
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Show main page after login has been submitted
+function showMainPage() {
+  formContainer.style.display = "none";
+  mainPage.style.display = "block";
+}
+
+// HighLighting button on selected section function
+function highlightButton(selectedButton: HTMLElement) {
+  const activeButton = document.querySelector(
+    ".icon-bar .active"
+  ) as HTMLElement;
+  if (activeButton) {
+    activeButton.classList.remove("active");
+  }
+  selectedButton.classList.add("active");
+}
+
 // Adding an event listener the form submit event
 // Using addEventlistener on submit button listenschecks if username and password is filled in
 form.addEventListener("submit", (event) => {
@@ -29,7 +53,7 @@ form.addEventListener("submit", (event) => {
   const username = usernameInput.value;
   const password = passwordInput.value;
 
-  // Check if form field contains are empty if empty throw error
+  // Check if form field containers are empty if empty throw error
   if (username.trim() === "" || password.trim() === "") {
     alert("Please fill in username!");
     return;
@@ -44,17 +68,34 @@ form.addEventListener("submit", (event) => {
   }
 
   // Logging the values to the console for reference
-  // Instead of loggin it, can be used to actually log in to page
+  // Instead of logging to console. It will be logged to a data base
   console.log(`Username: ${username}, Password: ${password}`);
 
-  // Hiding form simulating loggin in to user profile page
-  formContainer.style.display = "none";
-  header.style.display = "none";
-  // Redirecting to user profile page
-  // window.location.href = "about:blank";
+  //Show main page after submit button on login form has been clicked
+  showMainPage();
+
+  // Highlighting button on selected section
+  highlightButton(portfolioButton);
 });
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Event Listeners for each section to be highlighted when clicked or selected.
+// PORTFOLIO SELECTION
+portfolioButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  highlightButton(portfolioButton);
+});
+// ABOUT ME SELECTION
+aboutMeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  highlightButton(aboutMeButton);
+});
+// CONTACT ME SELECTION
+contactMeButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  highlightButton(contactMeButton);
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Adding an event listener to the submit button and click event
 // Creating the listener to listen/ check for when the user clicks the submit button
 submitButton.addEventListener("click", (event) => {
